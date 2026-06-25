038e4bff3338dc328a7e161d3ed3df87cef174f4
Isabel Rios <isabelrio1@upb.edu>
2026-06-25 18:36:18 -0400

    Win screen updated

--- donkeykong3P/Scripts/win_screen.gd
+++ donkeykong3P/Scripts/win_screen.gd
@@ -1,10 +1,34 @@
 extends Control
 
+const PRINCESS_FRAME_1 := preload("res://Assets/Princess.png")
+const PRINCESS_FRAME_2 := preload("res://Assets/Princess2.png")
+const HEART_FRAME_1 := preload("res://Assets/heart.png")
+const HEART_FRAME_2 := preload("res://Assets/heart2.png")
+
+@onready var princess: TextureRect = $CenterContainer/WinStack/CharacterStage/Princess
+@onready var heart: TextureRect = $CenterContainer/WinStack/CharacterStage/Heart
+@onready var mario: TextureRect = $CenterContainer/WinStack/CharacterStage/Mario
+
+var frame_index := 0
+
 
 func _ready() -> void:
 	SoundManager.stop_music()
 	SoundManager.play_win()
-	
+	_update_victory_animation()
+
+
+func _on_animation_timer_timeout() -> void:
+	frame_index = 1 - frame_index
+	_update_victory_animation()
+
+
+func _update_victory_animation() -> void:
+	var use_first_frame := frame_index == 0
+	princess.texture = PRINCESS_FRAME_1 if use_first_frame else PRINCESS_FRAME_2
+	heart.texture = HEART_FRAME_1 if use_first_frame else HEART_FRAME_2
+	mario.flip_h = not use_first_frame
+
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
